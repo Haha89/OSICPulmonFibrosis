5e773e337d67e4b1e3ed31cf284057f150c0e47a
haha89 <alexandre.blanchet@supelec.fr>
2020-07-14 15:22:36 +0200

    enter the commit message for your changes. Lines starting

--- lib/test.py
+++ lib/test.py
@@ -13,12 +13,26 @@ df = pd.concat([train, test])
 
 #Preprocessing
 print(df.head())
-#Transform Weeks, FVC, Percent, Age to a normal distribution
-for col in ["Weeks", "FVC", "Percent", "Age"]:
-    df[col] = (df[col] - df[col].min())/(df[col].max() - df[col].min())
-    df[col].plot(kind='hist')
-    plt.show()
 
 #Encode Sex and Smoker Status to one hot encoding
 df = pd.get_dummies(df, columns=['Sex', 'SmokingStatus'])
+
+# =============================================================================
+# Transform Weeks, FVC, Percent, Age to be in [0, 1]
+# =============================================================================
+# for col in ["Weeks", "FVC", "Percent", "Age"]:
+#     df[col] = (df[col] - df[col].min())/(df[col].max() - df[col].min())
+#     df[col].plot(kind='hist')
+#     plt.show()
+
+# =============================================================================
+# Transformation pour etre presque des lois normales TODO
+# =============================================================================
+from sklearn.preprocessing import PowerTransformer
+yj = PowerTransformer(method='yeo-johnson')
+
+
+
+
+
 print(df.head())  
