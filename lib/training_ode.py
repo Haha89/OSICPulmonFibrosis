ed2c30d321119008ae0e975e4846fac09f5ed6df
Brousseli <68235991+Brousseli@users.noreply.github.com>
2020-09-03 19:17:14 +0200

    Update training_ode.py

--- lib/training_ode.py
+++ lib/training_ode.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
             #TRAINING    
             for scans, misc, FVC, percent, weeks in training_generator:
                 ranger = np.where(weeks != 0)[1]
-                misc = misc[:,ranger[0],:] #Dépend du m
+                misc = misc[:,ranger[0],:].squeeze(1) #Dépend du m
                 fvc = FVC[:,ranger[0]]
                 percent = percent[:,ranger[0]]
                 weeks = weeks[:,ranger]
