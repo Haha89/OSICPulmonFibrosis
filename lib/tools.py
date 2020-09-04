ede6051eb3bac768343600d872dffbb90a6c6fae
Brousseli <68235991+Brousseli@users.noreply.github.com>
2020-09-04 11:39:24 +0200

    Update tools.py

--- lib/tools.py
+++ lib/tools.py
@@ -201,3 +201,17 @@ def laplace_log_likelihood(actual_fvc, predicted_fvc, confidence, mask):
     metric = (- sqrt(2) * delta / std_clipped - torch.log(sqrt(2) * std_clipped))*mask
     metric = metric.sum()/mask.sum()
     return -metric
+
+
+
+def ode_laplace_log_likelihood(actual_fvc, predicted_fvc, confidence):
+    """
+    Calculates the modified Laplace Log Likelihood score for this competition.
+    """
+    std_min = torch.tensor([70.]).cuda()
+    delta_max = torch.tensor([1000.]).cuda()
+    std_clipped = torch.max(confidence, std_min)
+    delta = torch.min(torch.abs(actual_fvc - predicted_fvc), delta_max)
+    metric = (- sqrt(2) * delta / std_clipped - torch.log(sqrt(2) * std_clipped))
+    metric = metric.mean()
+    return -metric
