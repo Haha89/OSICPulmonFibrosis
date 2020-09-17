9387e0dbab0757f3d3005a44f6b67d0735d9fab2
haha89 <alexandre.blanchet@supelec.fr>
2020-09-17 11:25:34 +0200

    update examples

--- lib/examples-001.py
+++ lib/examples-001.py
@@ -2,14 +2,18 @@
 
 """Examples to show what tools can do"""
 
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import pydicom
 import tools
+# print(tools)
 
 # =============================================================================
 # Display the list of CT scans of a patient
 # =============================================================================
 
-ID_PATIENT = "ID00132637202222178761324"
+ID_PATIENT = "ID00423637202312137826377"
 scan_filenames = tools.get_scans_from_id(ID_PATIENT)
 # print(f"List of CT scans found for id : {id}")
 # print(scan_filenames)
@@ -18,18 +22,18 @@ scan_filenames = tools.get_scans_from_id(ID_PATIENT)
 # # Display the content of one dcm file
 # # =============================================================================
 
-PATH_DATA = tools.get_path_id(ID_PATIENT)
-DATA_FILE = pydicom.dcmread(f"{PATH_DATA}/{scan_filenames[0]}")
-print("Content of the DCM file")
-print(DATA_FILE)
+# PATH_DATA = tools.get_path_id(ID_PATIENT)
+# DATA_FILE = pydicom.dcmread(f"{PATH_DATA}/{scan_filenames[0]}")
+# print("Content of the DCM file")
+# print(DATA_FILE)
 
 # =============================================================================
 # # Get the numpy 3d array already preprocessed
 # # =============================================================================
-# MATRIX = tools.get_3d_scan(ID_PATIENT)
+MATRIX = tools.get_3d_scan(ID_PATIENT)
 
 # # =============================================================================
 # # Annimation of the 3d matrix slice by slice (run from console)
 # # =============================================================================
-# print("Scroll to animate")
-# tools.multi_slice_viewer(MATRIX)
+print("Scroll to animate")
+tools.multi_slice_viewer(MATRIX)
---
+++ lib/examples-002.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Preprocessing of the Train dataset, display of content of one patient"""
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from tools import get_data, filter_data
+
+PREPROC = get_data()
+ID_PATIENT = "ID00026637202179561894768"
+OTHER, FVC, PERCENT, WEEKS = filter_data(PREPROC, ID_PATIENT)
+
+print(FVC)
+print(OTHER)
+print(PERCENT)
+print(WEEKS)
