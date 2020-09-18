dab2ea53ddd20fb9433300a2321a5949765ab34e
haha89 <alexandre.blanchet@supelec.fr>
2020-09-18 12:09:52 +0200

    generate submission file test

--- lib/submission.py
+++ lib/submission.py
@@ -10,11 +10,15 @@ from pickle import load
 from dataset import Dataset
 from torch.utils import data
 
+
+MIN_WEEK = -12
+MAX_WEEK = 130
 DEVICE = ("cuda" if torch.cuda.is_available() else "cpu")
 torch.cuda.empty_cache()
 
 test_df = utils.get_data(train=False)
 sub = pd.read_csv('../data/sample_submission.csv')
+sub = sub.iloc[0:0]
 
 checkpoint = torch.load('../data/model/model-0.pth')
 model = checkpoint['model']
@@ -37,7 +41,7 @@ testing_set = Dataset(a, train=False)
 testing_generator = data.DataLoader(testing_set, batch_size=1, shuffle=False)
 
 
-for scans, misc, FVC, percent, weeks in testing_generator:
+for i, (scans, misc, FVC, percent, weeks) in enumerate(testing_generator):
     
     ranger = np.where(weeks != 0)[1]
     misc = misc[:,ranger[0],:].squeeze(1) #Dépend du m
@@ -45,8 +49,7 @@ for scans, misc, FVC, percent, weeks in testing_generator:
     std = torch.ones_like(fvc)*.7
     fvc = torch.cat((fvc, std),1)
     percent = percent[:,ranger[0]]
-    weeks = weeks[:,ranger]
-    weeks = weeks - weeks[:,0]
+    weeks = torch.torch.from_numpy(np.arange(MIN_WEEK, MAX_WEEK+1)).float()
     scans, misc = scans.to(DEVICE), misc.to(DEVICE)
     fvc, percent, weeks = fvc.to(DEVICE), percent.to(DEVICE), weeks.to(DEVICE)
     pred = model(scans, misc, fvc, percent, weeks)
@@ -58,6 +61,11 @@ for scans, misc, FVC, percent, weeks in testing_generator:
     goal = unscale(goal).to(DEVICE)
     mean = mean  + (goal[:,0]- mean[:,0])
     
-    print(mean, std)
+    mean = mean.cpu().detach().numpy()[0]
+    std = std.cpu().detach().numpy()[0]
+    list_weeks = [test_df.Patient.unique()[i] + "_" + str(int(week.cpu().detach().numpy())) for week in weeks]
+
+    df = pd.DataFrame({"Patient_Week": list_weeks, "FVC":mean, "Confidence":std})
+    sub = pd.concat([sub, df], axis=0)
     
-# sub.rename(columns={"std":"FVC"})[["Patient_Week","FVC","Confidence"]].to_csv("../data/submission.csv", index=False)
+sub.to_csv("../data/submission.csv", index=False)
