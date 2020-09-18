7dc6fc978f2c283d5498ac8b55f686edd46754a2
haha89 <alexandre.blanchet@supelec.fr>
2020-09-18 11:27:54 +0200

    just to be up to date

--- lib/submission.py
+++ lib/submission.py
@@ -24,7 +24,7 @@ model.load_state_dict(checkpoint['state_dict'])
 
 with open('minmax.pickle', 'rb') as minmax_file:
     min_max = load(minmax_file)['FVC']
-    
+
 unscale = lambda x: x*(min_max["max"] - min_max["min"]) + min_max["min"]
 
 
@@ -35,18 +35,29 @@ unscale = lambda x: x*(min_max["max"] - min_max["min"]) + min_max["min"]
 a = np.arange(0, len(test_df.Patient.unique()))
 testing_set = Dataset(a, train=False)
 testing_generator = data.DataLoader(testing_set, batch_size=1, shuffle=False)
-print("Train Dataset OK")
+
 
 for scans, misc, FVC, percent, weeks in testing_generator:
     
+    ranger = np.where(weeks != 0)[1]
+    misc = misc[:,ranger[0],:].squeeze(1) #Dépend du m
+    fvc = FVC[:,ranger[0]]
+    std = torch.ones_like(fvc)*.7
+    fvc = torch.cat((fvc, std),1)
+    percent = percent[:,ranger[0]]
+    weeks = weeks[:,ranger]
+    weeks = weeks - weeks[:,0]
     scans, misc = scans.to(DEVICE), misc.to(DEVICE)
-    FVC, percent, weeks = FVC.to(DEVICE), percent.to(DEVICE), weeks.to(DEVICE)
-    pred = model(scans, misc, FVC, percent, weeks)
+    fvc, percent, weeks = fvc.to(DEVICE), percent.to(DEVICE), weeks.to(DEVICE)
+    pred = model(scans, misc, fvc, percent, weeks)
+    
+    #Deprocessing
     mean = unscale(pred[:, :, 0])
     std = pred[:, :, 1]*500    
-    print(mean)
-    print(std)
+    goal = FVC[:,ranger]
+    goal = unscale(goal).to(DEVICE)
+    mean = mean  + (goal[:,0]- mean[:,0])
     
-
+    print(mean, std)
     
 # sub.rename(columns={"std":"FVC"})[["Patient_Week","FVC","Confidence"]].to_csv("../data/submission.csv", index=False)
